cf32d447d106d0fcf8a70ec6379e5299c84669ec
Kevin Holmes <kevin.l.holmes@gmail.com>
2015-06-17 15:34:56 -0400

    Now possible to click on and click off X's in Tic Tac Toe cells.  Also using JQuery to give each cell a distinct ID.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,42 @@
 // Tic Tac Toe
 
-var board = [[" ", " ", " "],
-             [" ", " ", " "],
+var cellIds = [[" ", " ", " "],
+               [" ", " ", " "],
+               [" ", " ", " "]];
+
+var board = [[" ", " ", " "], 
+             [" ", " ", " "], 
              [" ", " ", " "]];
 
-var cellNumber = function (element) {
-    
-}
+var idCells = function () {
+    // This function needs to find each .cell element, and give them
+    // id's according to their row and position #r0c0, #r0c1, #r0c2,
+    // etc.  Also initialize cellIds and board arrays.
+        
+    $(".row").each(function(i,row) {
+        $(row).find(".cell").each(function(j,col) {
+            var id = "#r"+i.toString()+"c"+j.toString();
+            
+            $(col).attr("id", id);
+            cellIds[i][j] = id;
+            board[i][j] = " ";
+        });
+    });
+};
 
-var cellClick = function () {
-    console.log($(this).contents());
-    $(this).replaceWith('<div class="cell">X</div>');
+var cellClick = function (event) {
+    var id = $(this).attr("id");
+    var cellContent = $(this).text();
+    
+    if (cellContent==="X") {
+        $(this).text(" ");
+    } else {
+        $(this).text("X");
+    }
     console.log($(this).text());
 };
 
 $(".cell").click(cellClick);
-
+idCells();
+console.log(board);
+console.log(cellIds);
