2dc45f702e63afc3adec9dca694429791403eeae
Kevin Holmes <kevin.l.holmes@gmail.com>
2015-07-23 18:31:11 -0400

    Tic Tac Toe game basically complete.  Still needs to clear the board and start over when someone wins now.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,22 @@
 // Tic Tac Toe
 
+var randomCell = function() {
+    // Just return a random cell from the Tic-Tac-Toe board in id.
+    var horzCellNum = Math.floor( Math.random() * 3 );
+    var vertCellNum = Math.floor( Math.random() * 3 );
+    return coord2id(horzCellNum, vertCellNum);
+};
+
+var emptyCells = new Array(0);
+
+var remove = function(element, array) {
+    var index = array.indexOf(5);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    return array;
+}
+
 var extractNum = function(n) {
     return function(str) {
         return parseInt(str.charAt(n), 10);
@@ -31,82 +48,79 @@ var coord2id = function (coord0, coord1) {
     return id;
 };
 
-var isDiagWin = function (cellId, player) {
-    // Lazy method
-
-    // First define array of all diagonal win combinatons. There are
-    // only two.
-    var win = [[coord2id(0,0), coord2id(1,1), coord2id(2,2)],
+var diagWin = [[coord2id(0,0), coord2id(1,1), coord2id(2,2)],
                [coord2id(2,0), coord2id(1,1), coord2id(0,2)]];
-    console.log("Diag win: ", win);
 
-    // If all three cells are the players mark, then return true.
-    var allSame = true;
-    var someWin = false;
-
-    for (var i=0; i<win.length; i+=1) {
-        allSame = true;         // Reset allSame on each iteration
-        allSame = allSame && ($(win[i][0]).text() === player);
-        allSame = allSame && ($(win[i][1]).text() === player);
-        allSame = allSame && ($(win[i][2]).text() === player);
-        someWin = someWin || allSame;
-    }
-
-    return someWin;
-};
-
-var isVertWin = function (cellId, player) {
-    // Lazy method
-
-    // First define array of all vertical win combinatons. There are
-    // only three.
-    var win = [[coord2id(0,0), coord2id(1,0), coord2id(2,0)],
+var vertWin = [[coord2id(0,0), coord2id(1,0), coord2id(2,0)],
                [coord2id(0,1), coord2id(1,1), coord2id(2,1)],
                [coord2id(0,2), coord2id(1,2), coord2id(2,2)]];
-    console.log("Vert win: ", win);
 
-    // If all three cells are the players mark, then return true.
-    var allSame = true;
-    var someWin = false;
-
-    for (var i=0; i<win.length; i+=1) {
-        allSame = true;         // Reset allSame on each iteration
-        allSame = allSame && ($(win[i][0]).text() === player);
-        allSame = allSame && ($(win[i][1]).text() === player);
-        allSame = allSame && ($(win[i][2]).text() === player);
-        someWin = someWin || allSame;
-    }
-
-    return someWin;
-};
-
-var isHorzWin = function (cellId, player) {
-    // Lazy method                 
-
-    // First define array of all horizontal win combinatons. There are
-    // only three.
-    var win = [[coord2id(0,0), coord2id(0,1), coord2id(0,2)],
+var horzWin = [[coord2id(0,0), coord2id(0,1), coord2id(0,2)],
                [coord2id(1,0), coord2id(1,1), coord2id(1,2)],
                [coord2id(2,0), coord2id(2,1), coord2id(2,2)]];
 
-    console.log("Horz win: ", win);
+var searchCells = function (cellArray, player) {
+    // cellArray is a two-dimensional array.  Search each element in
+    // cellArray to see if each element in that sub-array is the same
+    // as player.  If so, return those cellIds in an array. Otherwise,
+    // return an empty array.
 
-    // If all three cells are the players mark, then return true.
     var allSame = true;
-    var someWin = false;
-
-    for (var i=0; i<win.length; i+=1) {
+    var matchedArray = [];
+    for (var i=0; i<cellArray.length; i+=1) {
         allSame = true;         // Reset allSame on each iteration
-        allSame = allSame && ($(win[i][0]).text() === player);
-        allSame = allSame && ($(win[i][1]).text() === player);
-        allSame = allSame && ($(win[i][2]).text() === player);
-        someWin = someWin || allSame;
+        allSame = allSame && ($(cellArray[i][0]).text() === player);
+        allSame = allSame && ($(cellArray[i][1]).text() === player);
+        allSame = allSame && ($(cellArray[i][2]).text() === player);
+        if (allSame) {
+            matchedArray.push(cellArray[i]);
+        }
     }
-
-    return someWin;
+    return matchedArray;
 };
 
-var isWin = function (cellId, player) {
+var isStringEmpty = function (str) {
+    // Found at http://stackoverflow.com/a/1173854
+    var isEmpty = ($.trim(str) === '');
+    
+    return isEmpty;
+};
+
+var searchEmptyCells = function () {
+    // return list of empty cells
+    var cells = [];
+    
+    $(".row").each(function(i,row) {
+        $(row).find(".cell").each(function(j,col) {
+            var id = "r"+i.toString()+"c"+j.toString();
+            var cellContent = $(col).text();
+            if (isStringEmpty(cellContent)) {
+                cells.push(id);
+            }
+        });
+    });
+    console.log("cells: ", cells);
+    return cells;
+};
+
+var cellClick = function () {
+    // Click handler
+
+    // We need the HTML id of the cell that was clicked.
+    var cellId = "#" + $(this).attr("id");
+
+    // We also need the text content of the clicked cell.
+    var cellContent = $(this).text();
+    
+    // If the cell is empty, change it to an X.
+    if (isStringEmpty(cellContent)) {
+        $(this).text("X");
+    } else {
+        return;
+    }
+
+    // Beginning of old function isWin
+    // Functionality in isWin should really go in here.
     // Return true if the player won horizontally, vertically, or
     // diagonally. Otherwise return false.
 
@@ -118,53 +132,80 @@ var isWin = function (cellId, player) {
 
     if (playerWin) {
         var matchCells = [];
+        // Concatenate the list of win cells from all possibilities.
         matchCells = matchCells.concat(searchCells(horzWin, "X"),
                                        searchCells(vertWin, "X"),
                                        searchCells(diagWin, "X"));
-        
+
+        // Add CSS "win" class to winning cells.
         for(var i=0; i<matchCells.length; i++) {
-            console.log("matchCells[i]: ", matchCells[i]);            
             $(matchCells[i][0]).addClass("win");
-            console.log("matchCells[i][0]: ", matchCells[i][0]);
             $(matchCells[i][1]).addClass("win");
-            console.log("matchCells[i][1]: ", matchCells[i][1]);
             $(matchCells[i][2]).addClass("win");
-            console.log("matchCells[i][2]: ", matchCells[i][2]);
         }
     }
-    
-    return playerWin;
-};
+    // End of old function isWin
 
-
-
-var cellClick = function () {
-    // Click handler
-
-    // We need the HTML id of the cell that was clicked.
-    var cellId = "#" + $(this).attr("id");
-
-    // We also need the text content of the clicked cell.
-    var cellContent = $(this).text();
-
-    var isStringEmpty = function (str) {
-        // Found at http://stackoverflow.com/a/1173854
-        var isEmpty = ($.trim(str) === '');
-
-        return isEmpty;
-    };
-    
-    // If the cell is empty, change it to an X.
-    if (isStringEmpty(cellContent)) {
-        $(this).text("X");
-    }
-
-    // Did the player win?  If so, just write "Win!" to the console
-    // for now.
-    if (isWin(cellId, "X")) {
+    // Did the player win?  If so, pop up an alert box for now.
+    if (playerWin) {
         console.log("Win!");
         alert("You win!");
     }
+
+    // Let's start the computer player (O) here.  Refactor later.
+
+    // Make a function that will get a list of empty spaces on the
+    // board.
+    var emptyCells = searchEmptyCells();
+    console.log("+emptyCells: ", emptyCells);
+    // If there are no empty spaces on the board, and neither
+    // player has won (e.g. above), pick a random element from this
+    // list of empty spaces.
+    var gameIsDraw = (emptyCells.length === 0) && !playerWin;
+    console.log("gameIsDraw: ", gameIsDraw);
+    var selectedCell;
+    if (gameIsDraw) {
+        alert("Game is a draw!");
+    } else if (emptyCells.length !== 0) {
+        selectedCell =
+            emptyCells[Math.floor(emptyCells.length*Math.random())];
+        console.log("selectedCell: ", selectedCell);
+        // TODO: Make a random function to declutter the above line.
+        // Then place O in that space.
+        $("#"+selectedCell).text("O");
+    }
+    
+    // Then find out if player O wins, using a lot of the same code
+    // above.
+
+    var computerWin = false;
+
+    computerWin = computerWin || searchCells(horzWin, "O").length !== 0;
+    computerWin = computerWin || searchCells(vertWin, "O").length !== 0;
+    computerWin = computerWin || searchCells(diagWin, "O").length !== 0;
+
+    if (computerWin) {
+        var matchCells = [];
+        // Concatenate the list of win cells from all possibilities.
+        matchCells = matchCells.concat(searchCells(horzWin, "O"),
+                                       searchCells(vertWin, "O"),
+                                       searchCells(diagWin, "O"));
+
+        // Add CSS "lose" class to winning cells.
+        for(var i=0; i<matchCells.length; i++) {
+            $(matchCells[i][0]).addClass("lose");
+            $(matchCells[i][1]).addClass("lose");
+            $(matchCells[i][2]).addClass("lose");
+        }
+    }
+    // End of old function isWin
+
+    // Did the player lose?  If so, pop up an alert box for now.
+    if (computerWin) {
+        console.log("Win");
+        alert("You lost.");
+    }
+
 };
 
 var idCells = function () {
@@ -175,7 +216,7 @@ var idCells = function () {
     $(".row").each(function(i,row) {
         $(row).find(".cell").each(function(j,col) {
             var id = "r"+i.toString()+"c"+j.toString();
-            
+            emptyCells.push(id);
             $(col).attr("id", id);
         });
     });
@@ -183,9 +224,9 @@ var idCells = function () {
 
 // Initialize game
 
-// First bind cellClick as click handler.
+ // First bind cellClick as click handler.
 $(".cell").click(cellClick);
 
 // Then call idCells to add HTML id to each cell div.
 idCells();
-
+console.log("emptyCells: ", emptyCells);
